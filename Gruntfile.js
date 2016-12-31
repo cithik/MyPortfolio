136b5abaf053b035dbc5daa5842dac4b17ecb533
cithik <cithik@yahoo.co.in>
2016-12-30 23:27:41 -0800

    final changes
    done

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,68 @@
+/*
+ After making necessary changes in the settings under responsive_images
+ run this with one of these options:
+ "grunt" alone creates a new, completed images directory
+ "grunt clean" removes the images directory
+ "grunt responsive_images" re-processes images without removing the old ones
+ */
+
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    responsive_images: {
-      dev: {
-        options: {
-          engine: 'im',
-          sizes: [{
-            name: 'small',
-            width: '30%',
-            suffix: '_small',
-            quality: 20
-          },{
-            name: 'medium',
-            width: '40%',
-            suffix: '_medium',
-            quality: 30
-          },{
-            name: 'large',
-            width: '50%',
-            suffix: '_large',
-            quality: 40
-          }]
+    grunt.initConfig({
+        responsive_images: {
+            dev: {
+                options: {
+                    engine: 'im',
+                    sizes: [{
+                        name:'small',
+                        width: '30%',
+                        height: '30%',
+                        suffix: '_small',
+                        quality: 60
+                    },{
+                        name:'medium',
+                        width: '50%',
+                        height: '50%',
+                        suffix: '_medium',
+                        quality: 80
+                    },{
+                        name:'large',
+                        width: '100%',
+                        height: '100%',
+                        suffix: '_large_1x',
+                        quality: 100
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png,jpeg}'],
+                    cwd: 'images_src/',
+                    dest: 'images/'
+                }]
+            }
         },
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'images_src/',
-          dest: 'images/'
-        }]
-      }
-    }
-  });
+        /* Clear out the images directory if it exists */
+        clean: {
+            dev: {
+                src: ['images']
+            }
+        },
 
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', ['responsive_images']);
+        /* Generate the images directory if it is missing */
+        mkdir: {
+            dev: {
+                options: {
+                    create: ['images']
+                }
+            }
+        }
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
 };
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ******** Description ******
+
  A portfolio website with responsive text and images, which could be rendered
  in any device,  was developed that will display images, descriptions and
  links to each of the portfolio projects I completed throughout the course of
  my Masters degree and Nanodegree program.
 
 ******* How to run ******
+
+Double click the index.html file to open in browser.
+Open dev tools to check responsiveness for all devices.
